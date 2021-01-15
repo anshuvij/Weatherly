cd928cf5eb272e3ba531f8211eb81068f5ffe3af
Anshu Vij <anshu.vij@sustlabs.com>
2021-01-15 20:59:28 +0530

    Initial Commit

---
+++ Weatherly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weatherly
+//
+//  Created by Mobile Apps Team on 1/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
